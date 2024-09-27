7ae171c4bb6fa0a724a51921a17d9dcb143e0fd5
evelincauane <104854560+evelincauane@users.noreply.github.com>
2024-09-27 16:50:13 +0000

    .

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,5 +1,5 @@
-async function quantidadeUsuarios() {
-    const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero-usuarios.json'
+async function quantidadeUsuariosPorRede() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
     const nomeDasRedes = Object.keys(dados)
@@ -20,5 +20,5 @@ async function quantidadeUsuarios() {
   
   }
   
-  quantidadeUsuarios()
+  quantidadeUsuariosPorRede()
   
